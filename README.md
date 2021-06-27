5dae5856632c92c1a2df51a30a2b8ce57b0be41c
tarang <tarangsinghal2@gmail.com>
2021-06-27 20:08:59 +0530

    commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # web-helper
+
+### features
+
+- A Simple HTML,CSS and JavaScript editor
+- Different themes available
+- Responsive Design
+- Easy to use
--- editor/editor.js
+++ editor/editor.js
@@ -3,21 +3,26 @@ const defaultHtml =
 <head>
     <title>Welcome</title>
     <style>
+
         *{
             box-sizing: border-box;
         }
+
         html{
             height: 100%;
         }
+
         body{
             height: 100%;
             width: 100%;
             margin: 0;
             padding: 10px;
         }
+
         p{
             color: red;
         }
+
     </style>
 </head>
 <body>
